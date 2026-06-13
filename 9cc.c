c7a03b416d582bfd4644db065c319fde72693b62
shion19990724 <wakishio0724@gmai.com>
2026-06-13 23:12:44 +0900

    wip: 一時保存（プログラム途中）

--- 9cc.c
+++ 9cc.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//　入力の終わりを表すトークンだったら１を返す
+bool at_eof(){
+	return token->kind == TK_EOF;
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -10,14 +16,21 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	char *p = argv[1];
+	//　トークナイズする
+	taken = tokenize(argv[1]);
 
+	//　アセンブリの前半部分を出力する
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("	mov rax, %ld\n", strtol(p, &p, 10));
 
-	while (*p)
+	//　式の最初は数でなければならないので、それをチェックして、
+	//　最初のmov命令を出力
+	printf("	mov rax, %ld\n", expect_number());
+
+	//　'+ <数>'あるいは'- <数>'というトークンの並びを消費しつつ
+	//	アセンブリを出力
+	while (!at_eof())
 	{
 		if (*p == '+')
 		{
